75dd129326c58f8b37188f0873dba174fc7f5e5d
yz <1126978661@qq.com>
2023-01-13 15:39:11 +0800

    update articles

---
+++ update_book.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+cd /Users/yz/work/github/blog-book/learning-basic
+mdbook build
+
+cd /Users/yz/work/github/blog-book/poetry
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-compile
+mdbook build
+
+cd /Users/yz/work/github/blog-book/poetry-engine
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-language
+mdbook build
+
+cd /Users/yz/work/github/blog-book/reading-technical
+mdbook build
+
+cd /Users/yz/work/github/blog-book/crafting-interpreters-zh
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-middleware
+mdbook build
+
+cd /Users/yz/work/github/blog-book/sanguozhi
+mdbook build
+
+cd /Users/yz/work/github/blog-book/elastic
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-network
+mdbook build
+
+cd /Users/yz/work/github/blog-book/shijing
+mdbook build
+
+cd /Users/yz/work/github/blog-book/elastic-101
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-os
+mdbook build
+
+cd /Users/yz/work/github/blog-book/work-reading
+mdbook build
+
+cd /Users/yz/work/github/blog-book/learning-algorithm
+mdbook build
+
+cd /Users/yz/work/github/blog-book/literature
+mdbook build
+
+cd /Users/yz/work/github/blog-book/毛选
+mdbook build
+
--- work-reading/src/SUMMARY.md
+++ work-reading/src/SUMMARY.md
@@ -5,7 +5,7 @@
 - [Official Docs](work_reading/introduce/introduce.md)
   - [datadog](article/introduce/datadog.md)
   
-Read Blogs
+# Read Blogs
 
 - [2022年阅读](work_reading/2022/2022.md)
   - [1~3月阅读文章](work_reading/2022/1_3.md)
